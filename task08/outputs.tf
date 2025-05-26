b170ebe4c25f4c5f0ed94b3a85b22152cb56f542
Raghav Jaiswal <raghav_jaiswal@epam.com>
2025-05-27 01:03:44 +0530

    commit

--- task08/outputs.tf
+++ task08/outputs.tf
@@ -7,6 +7,6 @@ output "aci_ip_address" {
   value = module.aci.aci_ip_address
 }
 
-output "cluster_ip" {
+output "aks_lb_ip" {
   value = data.kubernetes_service.k8_service.status[0].load_balancer[0].ingress[0].ip
 }
