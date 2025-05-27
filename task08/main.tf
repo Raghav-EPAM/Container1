dcad1eeef6ca3b64070f4c8c37ae26ddb3834e94
Raghav Jaiswal <raghav_jaiswal@epam.com>
2025-05-27 15:13:55 +0530

    commit

--- task08/main.tf
+++ task08/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "resource_group" {
   name     = local.rg_name
   location = var.location
@@ -75,8 +79,73 @@ module "aks" {
   key_vault_id   = module.keyvault.keyvault_id
 }
 
+provider "kubectl" {
+  host                   = yamldecode(module.aks.kube_config).clusters[0].cluster.server
+  client_certificate     = base64decode(yamldecode(module.aks.kube_config).users[0].user.client-certificate-data)
+  client_key             = base64decode(yamldecode(module.aks.kube_config).users[0].user.client-key-data)
+  cluster_ca_certificate = base64decode(yamldecode(module.aks.kube_config).clusters[0].cluster.certificate-authority-data)
+  load_config_file       = false
+}
+
+
+provider "kubernetes" {
+  host                   = yamldecode(module.aks.kube_config).clusters[0].cluster.server
+  client_certificate     = base64decode(yamldecode(module.aks.kube_config).users[0].user.client-certificate-data)
+  client_key             = base64decode(yamldecode(module.aks.kube_config).users[0].user.client-key-data)
+  cluster_ca_certificate = base64decode(yamldecode(module.aks.kube_config).clusters[0].cluster.certificate-authority-data)
+}
+
+resource "kubectl_manifest" "deployment" {
+  yaml_body = templatefile("${path.module}/k8s-manifests/deployment.yaml.tftpl", {
+    acr_login_server = "${local.acr_name}.azurecr.io"
+    app_image_name   = local.app_image_name
+    image_tag        = "latest"
+  })
+
+  wait_for {
+    field {
+      key   = "status.availableReplicas"
+      value = "1"
+    }
+  }
+  depends_on = [module.aks]
+}
+
+resource "kubectl_manifest" "service" {
+  wait_for {
+    field {
+      key        = "status.loadBalancer.ingress.[0].ip"
+      value      = "^(\\d+(\\.|$)){4}"
+      value_type = "regex"
+    }
+  }
+
+  yaml_body = file("${path.module}/k8s-manifests/service.yaml")
+
+  depends_on = [module.aks, kubectl_manifest.deployment]
+}
+
+resource "kubectl_manifest" "secret_provider" {
+  yaml_body = templatefile("${path.module}/k8s-manifests/secret-provider.yaml.tftpl", {
+    aks_kv_access_identity_id  = module.aks.aks_kv_access_identity_id
+    kv_name                    = local.keyvault_name
+    redis_url_secret_name      = local.redis_hostname_secret_name
+    redis_password_secret_name = local.redis_primary_key_secret_name
+    tenant_id                  = azurerm_user_assigned_identity.identity.tenant_id
+  })
+
+  depends_on = [module.aks, kubectl_manifest.deployment, module.redis, module.keyvault]
+}
+
 data "kubernetes_service" "k8_service" {
   metadata {
     name = "redis-flask-app-service"
   }
+  depends_on = [kubectl_manifest.service]
+}
+
+resource "azurerm_user_assigned_identity" "identity" {
+  location            = var.location # Place identity in same region as AKS cluster
+  name                = "identity"   # Friendly name for tracking in Azure portal
+  resource_group_name = var.rg_name  # Identity lives in the same RG as AKS
 }
--- task08/versions.tf
+++ task08/versions.tf
@@ -6,8 +6,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.110.0, < 4.0.0"
     }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
-}
-provider "azurerm" {
-  features {}
+
 }
