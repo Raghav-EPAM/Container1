d43cded7c9db45ead9c566eb72281cc0026cd07d
Raghav Jaiswal <raghav_jaiswal@epam.com>
2025-05-27 14:08:21 +0530

    commit

--- task08/outputs.tf
+++ task08/outputs.tf
@@ -3,11 +3,6 @@ output "aci_fqdn" {
   value       = module.aci.aci_fqdn
 }
 
-/*output "aci_ip_address" {
-  description = "ACI IP Address"
-  value       = module.aci.aci_ip_address
-}*/
-
 output "aks_lb_ip" {
   description = "Load Balancer IP address for AKS"
   value       = data.kubernetes_service.k8_service.status[0].load_balancer[0].ingress[0].ip
