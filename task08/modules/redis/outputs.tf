98c3dae1eb17f87041de6c724436901e7ff3aac9
Raghav Jaiswal <raghav_jaiswal@epam.com>
2025-06-01 02:30:51 +0530

    commit

--- task08/modules/redis/outputs.tf
+++ task08/modules/redis/outputs.tf
@@ -2,14 +2,3 @@ output "redis_id" {
   description = "ID of the Redis Cache"
   value       = azurerm_redis_cache.redis.id
 }
-
-output "redis_hostname" {
-  description = "Hostname of the Redis Cache"
-  value       = azurerm_redis_cache.redis.hostname
-}
-
-output "redis_primary_key" {
-  description = "Primary access key for the Redis Cache"
-  value       = azurerm_redis_cache.redis.primary_access_key
-  sensitive   = true
-}
